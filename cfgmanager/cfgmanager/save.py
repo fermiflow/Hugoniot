a1003f4b9563ce98afa1bd66ac2d1192d053a741
lizh <lzh@iphy.ac.cn>
2026-05-14 17:49:03 +0800

    rm save git and version in cfgmanager

--- cfgmanager/cfgmanager/save.py
+++ cfgmanager/cfgmanager/save.py
@@ -1,19 +1,15 @@
 import os
-import git
 from datetime import datetime
 from omegaconf import DictConfig, OmegaConf, open_dict
 
-from cfgmanager import __version__
 from cfgmanager.utils import *
 
 def save(cfg: DictConfig,
-         path: str, 
+         path: str,
          cfg_save_filename: str = "config.yaml",
          auto_save_path: bool = True,
          auto_save_time: bool = True,
          auto_save_time_format: str = "%Y-%m-%d_%H:%M:%S",
-         auto_save_git: bool = True,
-         auto_save_cfgmanager_version: bool = True,
          silent_mode: bool = False,
     ) -> str:
     """
@@ -25,8 +21,6 @@ def save(cfg: DictConfig,
             auto_save_path: (bool), if save path to config file. Default is True.
             auto_save_time: (bool), if save time to config. Default is True.
             auto_save_time_format: (str), auto save time format. Default is "%Y-%m-%d_%H:%M:%S".
-            auto_save_git: (bool), if save git info to config. Default is True.
-            auto_save_cfgmanager_version: (bool), if save cfgmanager version to config. Default is True.
             silent_mode: (bool), if run in silent mode. Default is False.
         OUTPUT:
             cfg_filename: (str), saved config file name.
@@ -37,11 +31,6 @@ def save(cfg: DictConfig,
     auto_save_path_key = "auto_save_path"
     auto_save_call_path_key = "call_path"
     auto_save_cfg_path_key = "cfg_path"
-    auto_save_git_key = "auto_save_git"
-    auto_save_git_branch_key = "branch"
-    auto_save_git_hash_key = "hash"
-    auto_save_git_message_key = "message"
-    auto_save_cfgmanager_version_key = "auto_save_cfgmanager_version"
 
     # get current time
     now = datetime.now()
@@ -117,71 +106,6 @@ def save(cfg: DictConfig,
             with open_dict(cfg):
                 cfg[auto_save_path_key][auto_save_cfg_path_key] = auto_save_cfg_path
         
-    # auto save git
-    if auto_save_git:
-        try:
-            repo = git.Repo(search_parent_directories=True)
-        except Exception:
-            print(f"{RED}Error occurred while getting git info: {Exception}{RESET}")
-
-        if not auto_save_git_key in cfg:
-            OmegaConf.set_struct(cfg, True)
-            with open_dict(cfg):
-                cfg[auto_save_git_key] = {}
-
-        # auto save git branch
-        if auto_save_git_branch_key in cfg[auto_save_git_key]:
-            old_branch = cfg[auto_save_git_key][auto_save_git_branch_key]
-            new_branch = repo.active_branch.name
-            if old_branch != new_branch:
-                cfg[auto_save_git_key][auto_save_git_branch_key] = new_branch
-                if not silent_mode:
-                    print(f"{YELLOW}change{RESET} {auto_save_git_branch_key}: {old_branch} -> {new_branch}")
-        else:
-            OmegaConf.set_struct(cfg, True)
-            with open_dict(cfg):
-                cfg[auto_save_git_key][auto_save_git_branch_key] = repo.active_branch.name
-
-        # auto save git hash
-        if auto_save_git_hash_key in cfg[auto_save_git_key]:
-            old_hash = cfg[auto_save_git_key][auto_save_git_hash_key]
-            new_hash = repo.head.commit.hexsha
-            if old_hash != new_hash:
-                cfg[auto_save_git_key][auto_save_git_hash_key] = new_hash
-                if not silent_mode:
-                    print(f"{YELLOW}change{RESET} {auto_save_git_hash_key}: {old_hash} -> {new_hash}")
-        else:
-            OmegaConf.set_struct(cfg, True)
-            with open_dict(cfg):
-                cfg[auto_save_git_key][auto_save_git_hash_key] = repo.head.commit.hexsha
-
-        # auto save git message
-        if auto_save_git_message_key in cfg[auto_save_git_key]:
-            old_message = cfg[auto_save_git_key][auto_save_git_message_key]
-            new_message = repo.head.commit.message
-            if old_message != new_message:
-                cfg[auto_save_git_key][auto_save_git_message_key] = new_message
-                if not silent_mode:
-                    print(f"{YELLOW}change{RESET} {auto_save_git_message_key}: {old_message} -> {new_message}")
-        else:
-            OmegaConf.set_struct(cfg, True)
-            with open_dict(cfg):
-                cfg[auto_save_git_key][auto_save_git_message_key] = repo.head.commit.message.strip()
-        
-    # auto save cfgmanager version
-    if auto_save_cfgmanager_version:
-        cfgmanager_version = __version__
-        if auto_save_cfgmanager_version_key in cfg:
-            old_cfgmanager_version = cfg[auto_save_cfgmanager_version_key]
-            if old_cfgmanager_version != cfgmanager_version:
-                cfg[auto_save_cfgmanager_version_key] = cfgmanager_version
-                if not silent_mode:
-                    print(f"{YELLOW}change{RESET} {auto_save_cfgmanager_version_key}: {old_cfgmanager_version} -> {cfgmanager_version}")
-        else:
-            OmegaConf.set_struct(cfg, True)
-            with open_dict(cfg):
-                cfg[auto_save_cfgmanager_version_key] = cfgmanager_version
-
     # save config
     OmegaConf.save(config=cfg, f=cfg_filename)
     if not silent_mode:
